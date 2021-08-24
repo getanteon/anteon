078563e612b58e46a222768479a2f12119531160
Kürşat AKTAŞ <kursat.ce@gmail.com>
2021-08-24 09:28:47 +0000

    tlsHandshake duration added

--- src/hammer/core/scenario/requester/http.go
+++ src/hammer/core/scenario/requester/http.go
@@ -84,30 +84,38 @@ func (h *httpRequester) Send(proxyAddr *url.URL) (res *types.ResponseItem) {
 	var contentLength int64
 	var requestErr types.RequestError
 
-	var dnsStart, connStart, resStart, reqStart, delayStart time.Time
-	var dnsDuration, connDuration, resDuration, reqDuration, delayDuration time.Duration
+	var dnsStart, connStart, tlsHandshakeStart, resStart, reqStart, delayStart time.Time
+	var dnsDur, connDur, tlsHandshakeDur, resDur, reqDur, delayDur time.Duration
 	trace := &httptrace.ClientTrace{
 		DNSStart: func(info httptrace.DNSStartInfo) {
 			dnsStart = time.Now()
 		},
 		DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
-			dnsDuration = time.Since(dnsStart)
+			dnsDur = time.Since(dnsStart)
 		},
 		GetConn: func(h string) {
 			connStart = time.Now()
 		},
 		GotConn: func(connInfo httptrace.GotConnInfo) {
 			if !connInfo.Reused {
-				connDuration = time.Since(connStart)
+				connDur = time.Since(connStart)
 			}
 			reqStart = time.Now()
 		},
+		TLSHandshakeStart: func() {
+			tlsHandshakeStart = time.Now()
+		},
+		TLSHandshakeDone: func(cs tls.ConnectionState, e error) {
+			if cs.HandshakeComplete && !cs.DidResume {
+				tlsHandshakeDur = time.Since(tlsHandshakeStart)
+			}
+		},
 		WroteRequest: func(w httptrace.WroteRequestInfo) {
-			reqDuration = time.Since(reqStart)
+			reqDur = time.Since(reqStart)
 			delayStart = time.Now()
 		},
 		GotFirstResponseByte: func() {
-			delayDuration = time.Since(delayStart)
+			delayDur = time.Since(delayStart)
 			resStart = time.Now()
 		},
 	}
@@ -115,6 +123,9 @@ func (h *httpRequester) Send(proxyAddr *url.URL) (res *types.ResponseItem) {
 	if proxyAddr != nil {
 		h.client.Transport.(*http.Transport).Proxy = http.ProxyURL(proxyAddr) // bind ProxyService.GetNewProxy at init method?
 	}
+	defer func() {
+		h.client.Transport.(*http.Transport).Proxy = nil
+	}()
 
 	httpReq := h.request.Clone(context.TODO())
 	// httpReq.URL.RawQuery += uuid.NewString() // TODO: this can be a feature. like -cache_bypass flag?
@@ -123,7 +134,7 @@ func (h *httpRequester) Send(proxyAddr *url.URL) (res *types.ResponseItem) {
 
 	start := time.Now()
 	httpRes, err := h.client.Do(httpReq)
-	resDuration = time.Since(resStart)
+	resDur = time.Since(resStart)
 	duration := time.Since(start)
 
 	if err != nil {
@@ -141,10 +152,6 @@ func (h *httpRequester) Send(proxyAddr *url.URL) (res *types.ResponseItem) {
 		httpRes.Body.Close()
 	}
 
-	defer func() {
-		h.client.Transport.(*http.Transport).Proxy = nil
-	}()
-
 	res = &types.ResponseItem{
 		ScenarioItemID: h.packet.ID,
 		RequestID:      uuid.New(),
@@ -154,13 +161,17 @@ func (h *httpRequester) Send(proxyAddr *url.URL) (res *types.ResponseItem) {
 		ContentLenth:   contentLength,
 		Err:            requestErr,
 		Custom: map[string]interface{}{
-			"dnsDuration":   dnsDuration,
-			"connDuration":  connDuration,
-			"reqDuration":   reqDuration,
-			"resDuration":   resDuration,
-			"delayDuration": delayDuration,
+			"dnsDuration":   dnsDur,
+			"connDuration":  connDur,
+			"reqDuration":   reqDur,
+			"resDuration":   resDur,
+			"delayDuration": delayDur,
 		},
 	}
+	if h.packet.Protocol == types.ProtocolHTTPS {
+		res.Custom["tlsDuration"] = tlsHandshakeDur
+	}
+
 	return
 }
 
