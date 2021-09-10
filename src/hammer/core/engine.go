c19e5393b174c413ee505b3946fde9463498ab61
Kürşat AKTAŞ <kursat.ce@gmail.com>
2021-09-10 22:58:33 +0300

    Waved attack type added

--- src/hammer/core/engine.go
+++ src/hammer/core/engine.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"reflect"
 	"sync"
 	"time"
 
@@ -133,12 +134,49 @@ func (e *engine) initReqCountArr() {
 		case types.LoadTypeWaved:
 			e.createWavedReqCountArr()
 		}
-		// fmt.Println(e.reqCountArr)
 	}
 }
 
 func (e *engine) createLinearReqCountArr() {
 	createLinearDistArr(e.hammer.TotalReqCount, e.reqCountArr)
+}
+
+func (e *engine) createIncrementalReqCountArr() {
+	steps := createIncrementalDistArr(e.hammer.TotalReqCount, e.hammer.TestDuration)
+	tickPerSecond := int(time.Second / (tickerInterval * time.Millisecond))
+	for i := range steps {
+		tickArrStartIndex := i * tickPerSecond
+		tickArrEndIndex := tickArrStartIndex + tickPerSecond
+		segment := e.reqCountArr[tickArrStartIndex:tickArrEndIndex]
+		createLinearDistArr(steps[i], segment)
+	}
+}
+
+func (e *engine) createWavedReqCountArr() {
+	tickPerSecond := int(time.Second / (tickerInterval * time.Millisecond))
+	quarterWaveCount := int((math.Log2(float64(e.hammer.TestDuration))))
+	qWaveDuration := int(e.hammer.TestDuration / quarterWaveCount)
+	reqCountPerQWave := int(e.hammer.TotalReqCount / quarterWaveCount)
+	tickArrStartIndex := 0
+
+	for i := 0; i < quarterWaveCount; i++ {
+		if i == quarterWaveCount-1 {
+			// Add remaining req count to the last wave
+			reqCountPerQWave += e.hammer.TotalReqCount - (reqCountPerQWave * quarterWaveCount)
+		}
+
+		steps := createIncrementalDistArr(reqCountPerQWave, qWaveDuration)
+		if i%2 == 1 {
+			reverse(steps)
+		}
+
+		for j := range steps {
+			tickArrEndIndex := tickArrStartIndex + tickPerSecond
+			segment := e.reqCountArr[tickArrStartIndex:tickArrEndIndex]
+			createLinearDistArr(steps[j], segment)
+			tickArrStartIndex += tickPerSecond
+		}
+	}
 }
 
 func createLinearDistArr(count int, arr []int) {
@@ -155,10 +193,10 @@ func createLinearDistArr(count int, arr []int) {
 	}
 }
 
-func (e *engine) createIncrementalReqCountArr() {
-	steps := make([]int, e.hammer.TestDuration)
-	sum := (e.hammer.TestDuration * (e.hammer.TestDuration + 1)) / 2
-	incrementStep := int(math.Ceil(float64(sum) / float64(e.hammer.TotalReqCount)))
+func createIncrementalDistArr(count int, len int) []int {
+	steps := make([]int, len)
+	sum := (len * (len + 1)) / 2
+	incrementStep := int(math.Ceil(float64(sum) / float64(count)))
 	val := 0
 	for i := range steps {
 		if i > 0 {
@@ -172,28 +210,33 @@ func (e *engine) createIncrementalReqCountArr() {
 		}
 	}
 
-	sum = 0
+	sum = arraySum(steps)
+
+	factor := count / sum
+	remaining := count - (sum * factor)
+	plus := remaining / len
+	lastRemaining := remaining - (plus * len)
+	for i := range steps {
+		steps[i] = steps[i]*factor + plus
+		if len-i-1 < lastRemaining {
+			steps[i]++
+		}
+	}
+	return steps
+}
+
+func arraySum(steps []int) int {
+	sum := 0
 	for i := range steps {
 		sum += steps[i]
 	}
-
-	factor := e.hammer.TotalReqCount / sum
-	remaining := e.hammer.TotalReqCount - (sum * factor)
-	plus := remaining / len(steps)
-	lastRemaining := remaining - (plus * len(steps))
-	for i := range steps {
-		steps[i] = steps[i]*factor + plus
-		if len(steps)-i-1 < lastRemaining {
-			steps[i]++
-		}
-
-		tickArrStartIndex := i * 10
-		createLinearDistArr(steps[i], e.reqCountArr[tickArrStartIndex:tickArrStartIndex+10])
-	}
-
+	return sum
 }
 
-// TODO
-func (e *engine) createWavedReqCountArr() {
-	return
+func reverse(s interface{}) {
+	n := reflect.ValueOf(s).Len()
+	swap := reflect.Swapper(s)
+	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
+		swap(i, j)
+	}
 }
