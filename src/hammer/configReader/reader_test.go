235ba27234c89046bccf35ad6bb18e4ce3d6b0df
Kürşat AKTAŞ <kursat.ce@gmail.com>
2021-09-15 15:59:15 +0300

    config reader unit tests

---
+++ src/hammer/configReader/reader_test.go
@@ -0,0 +1,71 @@
+/*
+*
+*	Ddosify - Load testing tool for any web system.
+*   Copyright (C) 2021  Ddosify (https://ddosify.com)
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as published
+*   by the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Affero General Public License for more details.
+*
+*   You should have received a copy of the GNU Affero General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+ */
+
+package configReader
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewConfigReader(t *testing.T) {
+	t.Parallel()
+	config_path := "reader_testdata/config.json"
+	reader, err := NewConfigReader(config_path, ConfigTypeJson)
+
+	if err != nil {
+		t.Errorf("TestNewConfigReader errored: %v", err)
+	}
+
+	if reflect.TypeOf(reader) != reflect.TypeOf(&jsonReader{}) {
+		t.Errorf("Expected jsonReader found: %v", reflect.TypeOf(reader))
+	}
+}
+
+func TestNewConfigReaderInvalidConfigType(t *testing.T) {
+	t.Parallel()
+	config_path := "reader_testdata/config.json"
+	_, err := NewConfigReader(config_path, "invalidConfigType")
+
+	if err == nil {
+		t.Errorf("TestNewConfigReaderInvalidConfigType errored")
+	}
+
+}
+
+func TestNewConfigReaderInvalidFilePath(t *testing.T) {
+	t.Parallel()
+	config_path := "reader_testdata/invalid_file_path.json"
+	_, err := NewConfigReader(config_path, ConfigTypeJson)
+
+	if err == nil {
+		t.Errorf("TestNewConfigReaderInvalidFilePath errored")
+	}
+}
+
+func TestNewConfigReaderIncorrectJsonFile(t *testing.T) {
+	t.Parallel()
+	config_path := "reader_testdata/incorrect_formatted.json"
+	_, err := NewConfigReader(config_path, ConfigTypeJson)
+
+	if err == nil {
+		t.Errorf("TestNewConfigReaderInvalidFilePath errored")
+	}
+}
