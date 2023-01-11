7ace38b525c5e2a006995e609d80a1ce43edd477
Kenan Faruk Çakır <kenanfarukcakir@gmail.com>
2023-01-11 11:37:31 +0000

    add remote read from csv

--- config/csv.go
+++ config/csv.go
@@ -4,15 +4,20 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strconv"
 )
 
 func validateConf(conf CsvConf) error {
-	if conf.Order == "random" || conf.Order == "sequential" {
-		return nil
+	if !(conf.Order == "random" || conf.Order == "sequential") {
+		return fmt.Errorf("unsupported order %s, should be random|sequential", conf.Order)
 	}
-	return fmt.Errorf("unsupported order %s, should be random|sequential", conf.Order)
+	if !(conf.Src == "local" || conf.Src == "remote") {
+		return fmt.Errorf("unsupported src %s, should be local|remote", conf.Order)
+	}
+	return nil
 }
 
 func readCsv(conf CsvConf) ([]map[string]interface{}, error) {
@@ -21,78 +26,88 @@ func readCsv(conf CsvConf) ([]map[string]interface{}, error) {
 		return nil, err
 	}
 
+	var reader io.Reader
+
 	if conf.Src == "local" {
 		f, err := os.Open(conf.Path)
 		if err != nil {
 			return nil, err
 		}
+		reader = f
 		defer f.Close()
 
-		// read csv values using csv.Reader
-		csvReader := csv.NewReader(f)
-		csvReader.Comma = []rune(conf.Delimiter)[0]
-		csvReader.TrimLeadingSpace = true
-		csvReader.LazyQuotes = conf.AllowQuota
-
-		data, err := csvReader.ReadAll()
+	} else if conf.Src == "remote" {
+		req, err := http.NewRequest(http.MethodGet, conf.Path, nil)
 		if err != nil {
 			return nil, err
 		}
-
-		if conf.SkipFirstLine {
-			data = data[1:]
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
 		}
+		reader = resp.Body
+		defer resp.Body.Close()
+	}
 
-		rt := make([]map[string]interface{}, 0) // unclear how many empty line exist
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(reader)
+	csvReader.Comma = []rune(conf.Delimiter)[0]
+	csvReader.TrimLeadingSpace = true
+	csvReader.LazyQuotes = conf.AllowQuota
 
-		for _, row := range data {
-			if conf.SkipEmptyLine && emptyLine(row) {
-				continue
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	if conf.SkipFirstLine {
+		data = data[1:]
+	}
+
+	rt := make([]map[string]interface{}, 0) // unclear how many empty line exist
+
+	for _, row := range data {
+		if conf.SkipEmptyLine && emptyLine(row) {
+			continue
+		}
+		x := map[string]interface{}{}
+		for index, tag := range conf.Vars { // "0":"name", "1":"city","2":"team"
+			i, err := strconv.Atoi(index)
+			if err != nil {
+				return nil, err
 			}
-			x := map[string]interface{}{}
-			for index, tag := range conf.Vars { // "0":"name", "1":"city","2":"team"
-				i, err := strconv.Atoi(index)
+			// convert
+			var val interface{}
+			switch tag.Type {
+			case "json":
+				json.Unmarshal([]byte(row[i]), &val)
+			case "int":
+				var err error
+				val, err = strconv.Atoi(row[i])
 				if err != nil {
 					return nil, err
 				}
-				// convert
-				var val interface{}
-				switch tag.Type {
-				case "json":
-					json.Unmarshal([]byte(row[i]), &val)
-				case "int":
-					var err error
-					val, err = strconv.Atoi(row[i])
-					if err != nil {
-						return nil, err
-					}
-				case "float":
-					var err error
-					val, err = strconv.ParseFloat(row[i], 64)
-					if err != nil {
-						return nil, err
-					}
-				case "bool":
-					var err error
-					val, err = strconv.ParseBool(row[i])
-					if err != nil {
-						return nil, err
-					}
-				default:
-					val = row[i]
+			case "float":
+				var err error
+				val, err = strconv.ParseFloat(row[i], 64)
+				if err != nil {
+					return nil, err
 				}
-				x[tag.Tag] = val
+			case "bool":
+				var err error
+				val, err = strconv.ParseBool(row[i])
+				if err != nil {
+					return nil, err
+				}
+			default:
+				val = row[i]
 			}
-			rt = append(rt, x)
+			x[tag.Tag] = val
 		}
-
-		return rt, nil
-
-	} else if conf.Src == "remote" {
-		// TODOcorr, http call
+		rt = append(rt, x)
 	}
 
-	return nil, fmt.Errorf("csv read error")
+	return rt, nil
 }
 
 func emptyLine(row []string) bool {
