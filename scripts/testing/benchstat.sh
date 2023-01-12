df653748675d678e9349d54669d9e9662d2fe941
fatihbaltaci <baltacifatih14@gmail.com>
2023-01-12 15:42:49 +0000

    Update jenkinsfile

--- scripts/testing/benchstat.sh
+++ scripts/testing/benchstat.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 LIMIT=10
+IS_FAILED=0
 time_op=$(grep -A1 'time/op' gobench_branch_result.txt |tail -1 | awk '{print $8}' | tr -d + | tr -d %)
-echo "Max. Time op: $time_op / $LIMIT"
+echo -e "Max. Delta Time op: $time_op / $LIMIT\n" > benchstat.txt
 if (( $(echo "$time_op > $LIMIT" | bc -l) )); then
-    exit 1
+    IS_FAILED=1
 fi
 
 alloc_op=$(grep -A1 'alloc/op' gobench_branch_result.txt |tail -1 | awk '{print $8}' | tr -d + | tr -d %)
-echo "Max. Alloc op: $alloc_op / $LIMIT"
+echo -e "Max. Delta Alloc op: $alloc_op / $LIMIT\n" >> benchstat.txt
 if (( $(echo "$alloc_op > $LIMIT" | bc -l) )); then
-    exit 2
+    IS_FAILED=1
 fi
 
 allocs_op=$(grep -A1 'allocs/op' gobench_branch_result.txt |tail -1 | awk '{print $8}' | tr -d + | tr -d %)
-echo "Max. Allocs op: $allocs_op / $LIMIT"
+echo -e "Max. Delta Allocs op: $allocs_op / $LIMIT\n" >> benchstat.txt
 if (( $(echo "$allocs_op > $LIMIT" | bc -l) )); then
-    exit 3
+    IS_FAILED=1
+fi
+
+if [ $IS_FAILED -eq 1 ]; then
+    exit 1
 fi
