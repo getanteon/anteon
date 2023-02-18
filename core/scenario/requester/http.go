5b62cdd0296baf33754e3c7efeebde3efbc866fa
Kürşat Aktaş <kursat.ce@gmail.com>
2023-02-18 21:33:48 +0000

    another tls cipher test

--- core/scenario/requester/http.go
+++ core/scenario/requester/http.go
@@ -478,10 +478,7 @@ func (h *HttpRequester) initTLSConfig() *tls.Config {
 		MinVersion:             tls.VersionTLS12,
 		SessionTicketsDisabled: false,
 		ClientSessionCache:     tls.NewLRUClientSessionCache(10000),
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
+		CipherSuites:           []uint16{tls.TLS_AES_128_GCM_SHA256},
 	}
 
 	if h.packet.CertPool != nil && h.packet.Cert.Certificate != nil {
