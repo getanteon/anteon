66298fb7e24df66b23a71a0d5a8ca982ebdd38c2
fatihbaltaci <baltacifatih14@gmail.com>
2023-04-24 22:09:45 +0300

    Update

--- selfhosted/install.sh
+++ selfhosted/install.sh
@@ -85,6 +85,7 @@ fi
 
 echo "🚀 Deploying Ddosify Self Hosted..."
 $COMPOSE_COMMAND -f "$REPO_DIR/selfhosted/docker-compose.yml" up -d
+docker pull busybox:1.34.1 >/dev/null 2>&1
 echo "⏳ Waiting for services to be ready..."
 docker run --rm --network selfhosted_ddosify busybox:1.34.1 /bin/sh -c "until nc -z nginx 80 && nc -z backend 8008 && nc -z hammermanager 8001 && nc -z rabbitmq_celery 5672 && nc -z rabbitmq_job 5672 && nc -z postgres_selfhosted 5432 && nc -z influxdb 8086; do sleep 5; done"
 echo "✅ Ddosify Self Hosted installation complete!"
